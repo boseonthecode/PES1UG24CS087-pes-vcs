09e0b31afbd40f38e62f83f3d7dc19bcd01422c2
Aryaroop Bose <224219618+boseonthecode@users.noreply.github.com>
2026-04-20 09:19:03 +0530

    Phase 1: Implement object_write header and hashing logic

--- object.c
+++ object.c
@@ -94,9 +94,27 @@ int object_exists(const ObjectID *id) {
 //
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
-    // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
-    return -1;
+    const char *type_str;
+    if (type == OBJ_BLOB) type_str = "blob";
+    else if (type == OBJ_TREE) type_str = "tree";
+    else if (type == OBJ_COMMIT) type_str = "commit";
+    else return -1;
+
+    char header[64];
+    int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len);
+    
+    size_t full_len = header_len + 1 + len;
+    uint8_t *full_data = malloc(full_len);
+    if (!full_data) return -1;
+    
+    memcpy(full_data, header, header_len);
+    full_data[header_len] = '\0';
+    memcpy(full_data + header_len + 1, data, len);
+
+    compute_hash(full_data, full_len, id_out);
+    
+    free(full_data);
+    return 0;
 }
 
 // Read an object from the store.
